8db1a49af9179ef2330228a06dcfbfb7a5efeb07
Samuel Greloch <sgreloch@Zipcoders-MBP-4.hsd1.de.comcast.net>
2024-11-09 20:17:16 -0500

    done

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,26 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Test;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
+
+
+
+@Test
+
+   public void DogTest(){
+
+
+String dogName = "";
+int birthDate = (0);
+
+    }
+
+
+
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
 }
--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -10,6 +10,28 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public class CatTest {
+
+
+
+    @Test
+
+
+
+    public void nameTest(){
+
+
+        // Given
+
+        String expectedname = "Cat";
+
+
+        Cat name = new Cat();
+
+
+
+    }
+
+
     // TODO - Create tests for `void setName(String name)`
     // TODO - Create tests for `speak`
     // TODO - Create tests for `setBirthDate(Date birthDate)`
